09ae70b8d068388c740ea384fafdcab059c258bf
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2015-03-19 02:40:32 +0100

    ...3

--- tp2/padding.py
+++ tp2/padding.py
@@ -10,51 +10,107 @@ def xor(a, b):
        c.append(x ^ y)
    return c
 
+def last_byte(b, num_bloc, seed):
+	last_byte_res = {'status':""}
+	# val = "00000000000000{0:02x}".format(num_bloc)
+
+	# """ a surveiller, potentiel bug """
+	# b.__ixor__(Block(val.encode()))
+	# """ a surveiller, potentiel bug """
+
+	y = xor(str(b)[len(str(b))-1:].encode(), "01".encode())
+	y = base64.b16encode(y).decode()
+	parameters = {"value":y}
+	print(parameters)
+	try :
+		last_byte = server.query("/last-byte/philippe/" + seed + "/" +num_bloc, parameters)
+		print("last_byte : " ,last_byte)
+	except:
+		print("fail")
+
+	return last_byte_res
+
+
+def oracle(challenge, num_bloc, dechiffre=None):
+	iv = challenge['IV']
+	msg = challenge['ciphertext']
+	# print(msg)
+
+	msg = Message(challenge['ciphertext'])
+	result = {'status':""}
+	while result['status'] != 'OK' :
+		b = Block.random(16)
+		if num_bloc > 1 :
+			val = "00000000000000{0:02x}".format(num_bloc)
+
+			""" a surveiller, potentiel bug """
+			b.__ixor__(Block(val.encode()))
+			""" a surveiller, potentiel bug """
+
+			blocks = b.hex() + msg[len(msg)-1].hex()
+		else :
+			blocks = b.hex() + msg[len(msg)-1].hex()
+		parameters = {'ciphertext':blocks, 'IV':iv}
+		# print(parameters)
+		result = server.query('/oracle/philippe', parameters)
+		# print(result)
+	return b	
+
 server = Server('http://pac.bouillaguet.info/TP2/padding-attack')
 
 seed = sys.argv[1]
-# print('/challenge/philippe/' + sys.argv[1])
-last_byte = {}
-last_byte['status'] = ""
-while last_byte['status'] != 'OK':
+last_byte_res = {}
+last_byte_res['status'] = ""
+while last_byte_res['status'] != 'OK':
 	seed = int(seed) +1
 	seed = str(seed)
 	print("Seed : " , seed)
 	result = server.query('/challenge/philippe/' + seed)
 
-	# print(result)
-	iv = result['IV']
-	msg = result['ciphertext']
-	# print(msg[len(msg)-2:])
-	# print(msg)
+	b = oracle(result, 1)
+	last_byte(b, 1, seed)
 
-	result['status'] = ""
-	while result['status'] != 'OK' :	
-		b = Block.random(16)
-		last_block_len = len(msg) - len(str(b))
-		blocks = b.hex() + msg[last_block_len:]
-		parameters = {'ciphertext':blocks, 'IV':iv}
-		# print(parameters)
-		try :
-			result = server.query('/oracle/philippe', parameters)
-			# print(result)
-		except :
-			print("fail")	
+	# # print(result)
+	# iv = result['IV']
+	# msg = result['ciphertext']
+	# # print(msg[len(msg)-2:])
+	# # print(msg)
+
+	# result['status'] = ""
+	# while result['status'] != 'OK' :
+	# 	b = Block.random(16)
+	# 	last_block_len = len(msg) - len(str(b))
+	# 	blocks = b.hex() + msg[last_block_len:]
+	# 	parameters = {'ciphertext':blocks, 'IV':iv}
+	# 	# print(parameters)
+	# 	try :
+	# 		result = server.query('/oracle/philippe', parameters)
+	# 		# print(result)
+	# 	except :
+	# 		print("fail")	
 
 
-	y = xor(str(b)[len(str(b))-1:].encode(), "01".encode())
-	print(base64.b16encode(y).decode())
-	print("msg : \n", msg[last_block_len-len(str(b)):last_block_len])
-	x = xor(y, msg[last_block_len-len(str(b)):last_block_len].encode())
-	x = base64.b16encode(x).decode()
-	y = base64.b16encode(y).decode()
-	parameters = {"value":x}
-	print(parameters)
-	try :
-		last_byte = server.query("/last-byte/philippe/" + seed, parameters)
-		print("last_byte : " ,last_byte)
-	except:
-		print("fail")
+	# y = xor(str(b)[len(str(b))-1:].encode(), "01".encode())
+	# y = base64.b16encode(y).decode()
+	# parameters = {"value":y}
+	# print(parameters)
+	# try :
+	# 	last_byte = server.query("/last-byte/philippe/" + seed, parameters)
+	# 	print("last_byte : " ,last_byte)
+	# 	dernier_octet = xor(str(b)[len(str(b))-1:].encode(), msg[last_block_len-len(str(b)):last_block_len].encode()) 
+	# 	dernier_octet = base64.b16encode(dernier_octet).decode()
+	# 	parameters = {"value":y}
+	# 	print(parameters)
+	# 	# try :
+	# 	last_byte = server.query("/last-byte/philippe/" + seed + "/1", parameters)
+	# 	print("last_byte : " ,last_byte)			
+	# 	# except:
+	# 		# print("fail-2")
+	# except:
+	# 	print("fail")
+
+
+
 
 # result['status'] = ""
 # while result['status'] != 'OK' :	
@@ -73,7 +129,7 @@ while last_byte['status'] != 'OK':
 # 		print("fail")
 
 
-def calcul_octet(octet, seed, y):
+def calcul_octet(octet, seed):
 	result = server.query('/challenge/philippe/' + seed)
 	iv = result['IV']
 	msg = result['ciphertext']
