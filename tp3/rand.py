76c6e7602645c0644f32704f53e43a61ca44ae69
Mandrivia <antoine.philippe62@gmail.com>
2015-03-26 13:20:59 +0100

    tp3

--- tp3/rand.py
+++ tp3/rand.py
@@ -1,6 +1,18 @@
 from client import *
 
 
+def XGCD(a, b):
+	u = (1,0)
+	v = (0,1)
+	while b != 0:
+		q, r = divmod(a, b)
+		a = b
+		b = r
+		tmp = (u[0] - q*v[0], u[1] - q*v[1])
+		u = v
+		v = tmp
+	return a, u[0], u[1]
+
 
 def rand():
 	global next
@@ -20,5 +32,20 @@ print(iv)
 # rand() = (next / 2^16) mod 2^15
 
 # next = 2^16*U + L,
-res = pow(2, 16) * iv[0]
-print("{0:02b}".format(res))
+u = (iv[0]*pow(2,16))
+print(u)
+u = u % pow(2,15)
+print(u)
+res = ''
+for i in range(pow(2,16)):
+	if iv[1] == u + i:
+		res =  i
+
+if res == '':
+	for i in range(pow(2,16)):
+		if iv[1] == u + pow(2,16) +i:
+			res = i
+
+print(res)
+
+
